443e5f07a81c0098d434b80ec6da13572c231a61
Mike_DevOps <mike.gordievsky@gmail.com>
2021-10-12 21:24:31 +0300

    pipeline changes 1

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/contacts', function ()
+Route::get('/', function ()
     {
         return view('welcome');
         Route::resource('Contacts', 'ContactsController');
